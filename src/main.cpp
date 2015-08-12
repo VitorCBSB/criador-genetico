b23e4440e582c84039ee176763878dc66f0f9533
Vitor Coimbra de Oliveira <vitorcoliveira91@gmail.com>
2015-08-12 18:16:47 -0300

    Added needed header.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <array>
 
 int main(int argc, char* argv[]) {
 	if (argc != 5) {
 		std::cout << "Uso: " << argv[0] << " <num_inputs> <num_outputs> <le_num_in> <num_rows> <num_cols>" << std::endl;
 		return 0;
 	}
+
+	std::array<bool, 8> funcs = {
+		true, // AND
+		true, // OR
+		true, // XOR
+		true, // NOT
+		true, // NAND
+		true, // XNOR
+		true, // NOR
+		true  // BUF
+	};
 
 	return 0;
 }
